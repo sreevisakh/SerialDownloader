67395f4c5195e8169c64382366735d6d2207aa7f
Sreevisakh <mail@sreevisakh.com>
2016-07-03 09:49:34 +0530

    Changed to es6

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ var cheerio = require('./cheerio');
 var bodyParser = require('body-parser')
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 
-app.post('/getUrls', function (req, res) {
+app.post('/getUrls', (req, res) => {
     console.log(req);
-    cheerio.getResult(req.body.url, req.body.season).then(function (response) {
-        res.send(response);
-    }, function (error) {
-        res.status(500).send(error);
-    });
+    cheerio.getResult(req.body.url, req.body.season)
+        .then(
+            response => res.send(response),
+            error => res.status(500).send(error)
+        );
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(port, () => console.log(`Serial Downloader Listening on ${port}!`));
